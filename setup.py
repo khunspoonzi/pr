76e2fd0510e55c99ff4596867b16d6f562769cfc
khunspoonzi <khunspoonzi@gmail.com>
2020-02-02 00:17:42 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+# -------------------------------------------------------------------------------------
+# IMPORTS
+# -------------------------------------------------------------------------------------
+
+import setuptools
+
+
+# -------------------------------------------------------------------------------------
+# README
+# -------------------------------------------------------------------------------------
+
+with open("README.md") as f:
+    README = f.read()
+
+
+# -------------------------------------------------------------------------------------
+# SETUP
+# -------------------------------------------------------------------------------------
+
+setuptools.setup(
+    author="Khun Spoonzi",
+    author_email="khunspoonzi@gmail.com",
+    name="pr",
+    license="MIT",
+    description="A high-level shorthand print formatter for Python 3 or above",
+    version="v0.0.1",
+    long_description=README,
+    url="https://github.com/khunspoonzi/pr",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=[],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+)

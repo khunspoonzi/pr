18f722f888913fa6b1095a81d132abf69f97b848
khunspoonzi <khunspoonzi@gmail.com>
2020-02-15 13:18:05 +0700

    Increment minor version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     name="pr",
     license="MIT",
     description="A high-level shorthand print formatter for Python 3 or above",
-    version="v0.4.5",
+    version="v0.5.0",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/khunspoonzi/pr",
